9ad371699c918f3179ef95a1a0881429da24672b
Pablo Rebolledo Acosta <prebolledo@MacBook-Pro-de-Pablo.local>
2019-10-08 15:41:10 -0300

    modulo users

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
 import server from './server'
+import users from './users'
 
 //Run server
 server(
     //Registra rutas del servicio
     (app) => {
-        //Para ingresar a la raiz del api
-        app.route('/').get((eq, res, next)=>{
-            res.status(200).json({
-                message:'Api usuaios'
-            })
-        })    
+        
+        //Obtener todos los users
+        app.get('/',(req, res, next)=>{
+            res.status(200).json(users.all())
+        })
+
+        app.get('/:id',(req, res, next)=>{
+            const user = users.get(req.params.id)
+
+            if(user){
+                res.status(200).json(user) 
+            }else{
+                res.status(404).json(user) 
+            }
+            
+        })
+
+        app.delete('/:id',(req, res, next)=>{
+            const result = users.remove(req.params.id)
+
+            if(result){
+                res.status(200).json(result) 
+            }else{
+                res.status(500).json(result) 
+            }
+            
+        })     
+           
+        app.post('/',(req, res, next)=>{
+            res.status(200).json(users.create(req.body)) 
+        })        
     }
 )
-
---
+++ users/index.js
@@ -0,0 +1,33 @@
+import collection from './collection'
+
+const all = () => {
+    return collection
+}
+
+const get = (id) => {
+    const users = collection.filter(user => user.id == id)
+    let user = null
+    if(users.length > 0){
+        user = users[0]
+    }
+    return user
+}
+
+const create = (user) => {
+    user.id = collection.length + 1
+    collection.push(user)
+    const {id} = user
+    return {id}
+}
+
+const remove = (id) => {
+    let i = collection.findIndex((user) => user.id == id)
+    return i >= 0 && collection.splice(i,1).length > 0
+}
+
+export default {
+    all,
+    get,
+    create,
+    remove,
+}
