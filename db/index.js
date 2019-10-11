1fb875478a7641e180336b5eb8d8ce56d8fc0b62
Pablo Rebolledo Acosta <prebolledo@MacBook-Pro-de-Pablo.local>
2019-10-11 10:28:00 -0300

    mejorando proceso de coneccion a mongo

--- db/index.js
+++ db/index.js
@@ -1,25 +1,29 @@
 const mongoose = require('mongoose')
 
-const open = async () => {
-    console.log('MONGO::CONNECT::INIT')
-    await mongoose.connect("mongodb://mongo/training", {
-        useNewUrlParser: true,
-        useCreateIndex: true,        
-        autoReconnect:true,
-        useFindAndModify: false,
-        
-        poolSize: 100,
-        socketTimeoutMS: 480000,
-        keepAlive: 20,
-        connectTimeoutMS: 5000,
-        reconnectTries: 30,
-        reconnectInterval: 5000,   
-    })
-    console.log('MONGO::CONNECT::END')
-}
-
-open()
-
+console.log('MONGO::CONNECT::INIT')
+mongoose.connect("mongodb://mongo/training", {
+    useNewUrlParser: true,
+    useCreateIndex: true,        
+    autoReconnect:true,
+    useFindAndModify: false,
+    
+    poolSize: 100,
+    socketTimeoutMS: 480000,
+    keepAlive: 20,
+    connectTimeoutMS: 5000,
+    reconnectTries: 30,
+    reconnectInterval: 5000,   
+})
 const connection = mongoose.connection 
+
+mongoose.connection.on('connected', () => console.log('MONGO::CONNECT::END')) 
+mongoose.connection.on('error', (err) => console.log('MONGO::CONNECT::ERROR '+ err))
+mongoose.connection.on('disconnected',() => console.log('MONGO::DISCONNECT'))
+process.on('SIGINT', () =>  
+    mongoose.connection.close(() => { 
+        console.log('MONGO::DISCONNECT:THROUGH-APP-TERMINATION')
+        process.exit(0)
+    })
+)
 
 export { mongoose, connection }
